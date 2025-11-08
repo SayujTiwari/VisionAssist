1e3b88664f0e28c48af561c504443ce37bdf1d8c
SayujTiwari <tiwarisayuj@gmail.com>
2025-11-08 15:23:06 -0700

    changed ui a bit better

--- Interfaceapp/app/page.tsx
+++ Interfaceapp/app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import useMockSensorData from "@/hooks/mockSensorData";
 import VoiceAlert from "@/components/voiceAlert";
+import DistanceBars from "@/components/distanceBars";
+
 
 export default function Page() {
   const { front, left, alert } = useMockSensorData();
@@ -8,7 +10,8 @@ export default function Page() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen gap-6">
       <h1 className="text-3xl font-bold">VisionAssist Dashboard</h1>
-
+      
+      <DistanceBars front={front} left={left} />
       <div className="text-lg">
         <p>Front Distance: {front.toFixed(2)} m</p>
         <p>Left Distance: {left.toFixed(2)} m</p>
